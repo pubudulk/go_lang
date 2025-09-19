1bb2e9068010ad777f7567138201c55c231c23e0
Pubudu Wijeyaratne <pubudu.wijeyaratne@ifs.com>
2025-09-19 15:09:51 +0530

    Update connection string mongo db

--- internal/config/config.go
+++ internal/config/config.go
@@ -24,7 +24,7 @@ func Load() *Config {
 
 	config := &Config{
 		Port:         getEnvWithDefault("PORT", "8080"),
-		MongoURI:     getEnvWithDefault("MONGO_URI", "MONGO_URI=mongodb+srv://root:bfw9sJjTXxfleyu7@localdevincidents.voqqf7r.mongodb.net/localdevincidents?retryWrites=true&w=majority&ssl=true&sslInsecure=true"),
+		MongoURI:     getEnvWithDefault("MONGO_URI", "mongodb+srv://root:bfw9sJjTXxfleyu7@localdevincidents.voqqf7r.mongodb.net/localdevincidents?retryWrites=true&w=majority&ssl=true&sslInsecure=true"),
 		DatabaseName: getEnvWithDefault("DATABASE_NAME", "localdevincidents"),
 		Environment:  getEnvWithDefault("ENVIRONMENT", "development"),
 	}
